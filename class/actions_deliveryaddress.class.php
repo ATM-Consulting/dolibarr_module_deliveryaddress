d77d9dc591c58dcda19fb0133870403053bfbf63
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2014-02-10 10:39:08 +0100

    debug

--- class/actions_deliveryaddress.class.php
+++ class/actions_deliveryaddress.class.php
@@ -13,30 +13,37 @@ class ActionsDeliveryaddress
 		
  		if ($action == 'builddoc'
  			&& (in_array('ordersuppliercard',explode(':',$parameters['context']))
-				|| in_array('ordercard',explode(':',$parameters['context'])))
-			)
-		{
-			dol_include_once('/contact/class/contact.class.php');
-			dol_include_once('/core/lib/pdf.lib.php');
-
-			$TContacts = $object->liste_contact();
-			foreach($TContacts as $c) {
-				if($c['code'] == 'SHIPPING') {
-					$contact = new Contact($db);
-					$contact->fetch($c['id']);
-					$soc = new Societe($db);
-					$soc->fetch($c['socid']);
-					
-					$address = $langs->trans("DeliveryAddress").": \n";
-					$address.= !empty($contact->socname) ? $contact->socname."\n" : "";
-					$address.= pdf_build_address($langs, $mysoc, $soc, $contact, 1, 'target');
-					$address.= !empty($contact->phone_pro) ? "\n".$langs->transnoentities("Phone").": ".$langs->convToOutputCharset($contact->phone_pro) : "";
-					$address.= !empty($object->note_public) ? "\n" : "";
-					$object->note_public = $address.$object->note_public;
-					break;
+				|| in_array('ordercard',explode(':',$parameters['context']))))
+			{
+				
+				/*echo '<pre>';
+				print_r($object);
+				echo '</pre>'; exit;*/
+				
+				dol_include_once('/contact/class/contact.class.php');
+				dol_include_once('/core/lib/pdf.lib.php');
+	
+				$TContacts = $object->liste_contact();
+				foreach($TContacts as $c) {
+					if($c['code'] == 'SHIPPING') {
+						$contact = new Contact($db);
+						$contact->fetch($c['id']);
+						$soc = new Societe($db);
+						$soc->fetch($c['socid']);
+						
+						$address = $langs->trans("DeliveryAddress").": \n";
+						$address.= !empty($contact->socname) ? $contact->socname."\n" : "";
+						$address.= pdf_build_address($langs, $mysoc, $soc, $contact, 1, 'target');
+						$address.= !empty($contact->phone_pro) ? "\n".$langs->transnoentities("Phone").": ".$langs->convToOutputCharset($contact->phone_pro) : "";
+						$address.= !empty($object->note_public) ? "\n" : "";
+						
+						if(strpos($object->note_public, $address) === FALSE){
+							$object->note_public = $address.$object->note_public;
+						}
+						break;
+					}
 				}
 			}
-		}
 		
 		return 0;
 	}
