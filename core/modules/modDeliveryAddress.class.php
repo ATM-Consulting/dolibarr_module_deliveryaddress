8fc102e5da7c63e6b5ff06e0f64486e5edad1617
florian HENRY <florian.henry@atm-consuliting.fr>
2020-04-29 11:23:05 +0200

    add config separateor between adress and note

--- core/modules/modDeliveryAddress.class.php
+++ core/modules/modDeliveryAddress.class.php
@@ -111,7 +111,7 @@ class modDeliveryAddress extends DolibarrModules
 		// Example: $this->const=array(0=>array('MYMODULE_MYNEWCONST1','chaine','myvalue','This is a constant to add',1),
 		//                             1=>array('MYMODULE_MYNEWCONST2','chaine','myvalue','This is another constant to add',0, 'current', 1)
 		// );
-		$this->const = array();
+		$this->const = array(0=>array('DELIVERYADDRESS_SEPARATOR_BETWEEN_NOTES','chaine','returnChar1','',1));
 
 		// Array to add new pages in new tabs
 		// Example: $this->tabs = array('objecttype:+tabname1:Title1:mylangfile@deliveryaddress:$user->rights->deliveryaddress->read:/deliveryaddress/mynewtab1.php?id=__ID__',  	// To add a new tab identified by code tabname1
