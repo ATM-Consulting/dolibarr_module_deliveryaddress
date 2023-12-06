a32d2a030fda40a1b29b5832e2c693018d368de0
John BOTELLA <john.botella@atm-consulting.fr>
2023-12-06 16:28:55 +0100

    Update backport/v19/core/class/commonhookactions.class.php

--- backport/v19/core/class/commonhookactions.class.php
+++ backport/v19/core/class/commonhookactions.class.php
@@ -24,9 +24,9 @@ namespace deliveryaddress;
  */
 
 
-if (file_exists(DOL_DOCUMENT_ROOT . '/htdocs/core/class/commonhookactions.class.php')){
+if (file_exists(DOL_DOCUMENT_ROOT . '/core/class/commonhookactions.class.php')){
 
-	require_once DOL_DOCUMENT_ROOT . '/htdocs/core/class/commonhookactions.class.php';
+	require_once DOL_DOCUMENT_ROOT . '/core/class/commonhookactions.class.php';
 	/**
 	 *	Parent class of all other hook actions classes
 	 */
